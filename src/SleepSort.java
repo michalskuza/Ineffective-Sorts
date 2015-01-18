e5dd2d0932340fc5064af5ca53b0c94aed4c6dec
Michal <michal@allocationerror.com>
2015-01-18 11:18:37 +0100

    Sleep sort with CyclicBarrier

--- src/SleepSort.java
+++ src/SleepSort.java
@@ -1,4 +1,6 @@
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class SleepSort {
     public static void sleepSortAndPrint(int[] nums) {
@@ -20,9 +22,31 @@ public class SleepSort {
         }
     }
 
+    public static void sleepSortAndPrintCyclicBarrier(int[] nums) {
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(nums.length);
+
+        for (final int num : nums) {
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+
+                        Thread.sleep(num * 1000);
+                        System.out.println(num);
+                        cyclicBarrier.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+
     public static void main(String[] args) {
         int[] numbers = {3, 12, 17, 10, 4, 18, 8, 7, 1, 15, 16, 14, 20, 13, 2, 19, 9, 11, 6, 5};
 
-        sleepSortAndPrint(numbers);
+//        sleepSortAndPrint(numbers);
+        sleepSortAndPrintCyclicBarrier(numbers);
     }
 }
